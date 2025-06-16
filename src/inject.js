0b8e75a23190d41eb210ecd019da78a66be60e49
benn <benno.der.flo@gmail.com>
2025-06-16 14:52:33 +0200

    Add files via upload

---
+++ src/inject.js
@@ -0,0 +1,21 @@
+function convertToWritable (input) {
+    var lines = input.split("\n");
+    var output = "";
+    lines.forEach((cm,line)=>{
+        if(!cm.startsWith("debug;")){
+            output+= `<div class='cm-line'><span class='cm'>${cm}</span></div>`
+        }else{
+            document.querySelector(".cm-breakpoint-gutter").dispatchEvent(new MouseEvent("mousedown", {
+                bubbles: true,
+                clientX: 0,
+                clientY: 55+(16.9*line),
+                button: 0
+            }))
+        }
+    })
+    return output
+}
+setInterval(()=>{
+    document.querySelector(".cm-content").innerHTML = localStorage.getItem('pykara_script') ?  convertToWritable(localStorage.getItem('pykara_script')) : "";
+    document.querySelector("main > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)").max = 10.5;
+},300)
